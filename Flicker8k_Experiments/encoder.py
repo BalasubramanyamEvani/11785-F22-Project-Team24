e7348876ae4f5794fd50af2ed183b43743c4f91f
Ubuntu <ubuntu@ip-172-31-39-227.us-east-2.compute.internal>
2022-12-15 13:10:52 +0000

    final code push for flickr8k dataset, needs to be merged with main branch

--- Flicker8k_Experiments/encoder.py
+++ Flicker8k_Experiments/encoder.py
@@ -1,19 +1,15 @@
 import torch.nn as nn
-from torchvision.models import densenet161, resnet152, vgg19
+from torchvision.models import resnet152, vgg19
 
 
 class Encoder(nn.Module):
-    def __init__(self, network='vgg19'):
+    def __init__(self, network="vgg19"):
         super(Encoder, self).__init__()
         self.network = network
-        if network == 'resnet152':
+        if network == "resnet152":
             self.net = resnet152(pretrained=True)
             self.net = nn.Sequential(*list(self.net.children())[:-2])
             self.dim = 2048
-        elif network == 'densenet161':
-            self.net = densenet161(pretrained=True)
-            self.net = nn.Sequential(*list(list(self.net.children())[0])[:-1])
-            self.dim = 1920
         else:
             self.net = vgg19(pretrained=True)
             self.net = nn.Sequential(*list(self.net.features.children())[:-1])
--- Flicker8k_Experiments/exps/sat_flicker_inference.sh
+++ Flicker8k_Experiments/exps/sat_flicker_inference.sh
@@ -2,6 +2,6 @@ python test_flicker.py \
     --coco_token_to_index_path=./coco_word_dict.json \
     --coco_index_to_token_path=./inv_coco_word_dict.json \
     --network=$1 \
-    --model=./model/model_$1_10.pth \
+    --model=./model/finetune/model_$1_3.pth \
     --flicker_test_data_path=./flicker8k/prep-test.txt \
-    --img_src_dir=./flicker8k/adv_images/resnet152/PGD/eps_0.03137254901960784_alpha_0.00784313725490196
+    --img_src_dir=./flicker8k/Flicker8k_Dataset
